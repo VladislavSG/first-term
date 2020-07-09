20d7689661e1d35ffb7f899b337a56d56c2472cd
VladislavSG <VladislavSG@users.noreply.github.com>
2020-07-10 01:36:39 +0300

    many mistakes fixed

--- bigint-optimized/dynamic_buffer.h
+++ bigint-optimized/dynamic_buffer.h
@@ -2,45 +2,26 @@
 #define BIGINT_DYNAMIC_BUFFER_H
 
 #include <cstddef>
+#include <cstdint>
 #include <vector>
 
-template <typename T>
 struct dynamic_buffer {
-    std::vector<T> data;
+    std::vector<uint32_t> data;
 
-    dynamic_buffer() : ref_counter(1) {};
+    dynamic_buffer();
 
-    dynamic_buffer(size_t size) :
-        data(size),
-        ref_counter(1) {};
+    dynamic_buffer(size_t size);
 
-    dynamic_buffer(std::vector<T> vec) :
-            data(vec),
-            ref_counter(1) {};
+    dynamic_buffer(std::vector<uint32_t>& vec);
 
     dynamic_buffer(dynamic_buffer const&) = delete;
     dynamic_buffer& operator=(dynamic_buffer const&) = delete;
 
-    bool unique() {
-        return ref_counter == 1;
-    }
+    bool unique();
 
-    dynamic_buffer* makeCopy() {
-        ++ref_counter;
-        return this;
-    }
+    dynamic_buffer* makeCopy();
 
-    void reduceCounter() {
-        if (--ref_counter == 0) {
-            delete this;
-        }
-    }
-
-    friend void swap(dynamic_buffer& a, dynamic_buffer& b) {
-        using std::swap;
-        swap(a.data, b.data);
-        swap(a.ref_counter, b.ref_counter);
-    }
+    void reduceCounter();
 private:
     size_t ref_counter;
 };
